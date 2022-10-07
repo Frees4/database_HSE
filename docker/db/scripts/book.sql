ae212dade56697d4a991bddfacb4b0134bbf1acc
Ilya Obrubov <iiobrubov@edu.hse.ru>
2022-10-07 23:25:10 +0300

    Delete docker/db/scripts directory

--- docker/db/scripts/book.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE book (
-    book_id INT PRIMARY KEY,
-    isbn_num INT,
-    year DATE,
-    book_name VARCHAR (50) NOT NULL,
-    book_author VARCHAR (50),
-    book_pages_num INT,
-    categories VARCHAR (50)
-);
--- docker/db/scripts/reader.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE reader (
-    reader_id INT PRIMARY KEY,
-    surname VARCHAR (50),
-    name VARCHAR (50),
-    adress VARCHAR (50),
-    birthday DATE
-);
